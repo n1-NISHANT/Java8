06139714bbcf1e64ed1734f6e19f00a8250575cc
n1-NISHANT <nk98nishant@gmail.com>
2025-01-14 09:17:23 +0530

    Update FindName.java
    Hear there is one array which contains names we will find name start with S by using Lambda Expression.

--- Basic/Lambda/FindName.java
+++ Basic/Lambda/FindName.java
@@ -1,1 +1,16 @@
+package Java8.Basic.Lambda;
 
+import java.util.function.Predicate;
+
+public class FindName {
+	public static void main(String[] args) {
+		
+		String [] arr = {"Sonu","Sandeep","Amit","nishant","abhisheak","santosh","Ajeet"};
+			  Predicate<String> p = (s1)->s1.toUpperCase().charAt(0)=='S';
+		  for(String s : arr) 
+		  { 
+			  if(p.test(s)==true)
+			  System.out.println(s); 
+		  }
+  }
+}
