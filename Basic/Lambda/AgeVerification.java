8cd6fe4b83f503e0d011a04dc0ad516ada8690fe
n1-NISHANT <nk98nishant@gmail.com>
2025-01-15 22:56:03 +0530

    Update AgeVerification.java
    In this we are Verifying Age is 18 or above and print name as a output Using Lambda Expiration

--- Basic/Lambda/AgeVerification.java
+++ Basic/Lambda/AgeVerification.java
@@ -1,1 +1,35 @@
+package Java8.Basic.Lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+class Person{
+	String name;
+	int age;
+	
+	public Person(String name,int age ) {
+		this.name = name;
+		this.age = age;
+	}
+}
+public class AgeVerification {
+	public static void main(String[] args) {
+		Person p1 = new Person("Sahil",25);
+		Person p2 = new Person("Salandra",12);
+		Person p3 = new Person("Anikate",18);
+		Person p4 = new Person("Gautam",10);
+		Person p5 = new Person("Viveak",33);
+		
+		List<Person> L1 =   Arrays.asList(p1,p2,p3,p4,p5);
+		
+		   Predicate<Person> p  =  person -> person.age>=18;
+		   
+		   for(Person person : L1) {
+			   if(p.test(person)== true) {
+				   System.out.println(person.name);
+			   }
+		   }
+	}
+}
 
