03e9aab86804c97f3d09b16e26e0819dc2cfa82c
n1-NISHANT <nk98nishant@gmail.com>
2025-01-16 23:52:34 +0530

    Update OddEven.java
    Using Scanner to take input from console. To print Even or not by using Lambda Functional Interface Predicate.

--- Basic/Lambda/OddEven.java
+++ Basic/Lambda/OddEven.java
@@ -1,11 +1,14 @@
 package Java8.Basic.Lambda;
 
+import java.util.Scanner;
 import java.util.function.Predicate;
 
 public class OddEven{
  public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
   Predicate<Integer> e = (a)-> a%2==0;
-		 System.out.println("given number is even --> "+e.test(10));
+	 // System.out.println("given number is even --> "+e.test(10));
+	  System.out.println("given number is even --> "+e.test(sc.nextInt()));
 }
 }
 
