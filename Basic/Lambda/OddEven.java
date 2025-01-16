942f9d10761b31d739b6a6d881d75c95b0b91390
n1-NISHANT <nk98nishant@gmail.com>
2025-01-16 23:47:01 +0530

    Update OddEven.java
    Odd Even by Using Predicate Functional Interface Lambda Expressions.

--- Basic/Lambda/OddEven.java
+++ Basic/Lambda/OddEven.java
@@ -1,1 +1,11 @@
+package Java8.Basic.Lambda;
+
+import java.util.function.Predicate;
+
+public class OddEven{
+ public static void main(String[] args) {
+  Predicate<Integer> e = (a)-> a%2==0;
+		 System.out.println("given number is even --> "+e.test(10));
+}
+}
 
