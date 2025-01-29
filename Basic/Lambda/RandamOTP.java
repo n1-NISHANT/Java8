e619514470d76e2853018ef85e82bb86d4891b36
n1-NISHANT <nk98nishant@gmail.com>
2025-01-29 20:32:28 +0530

    Update RandamOTP.java
    Generating Random OTP of 3 digest Using Supplier Functional Interface. (Supplier does not take input only returns value.)

--- Basic/Lambda/RandamOTP.java
+++ Basic/Lambda/RandamOTP.java
@@ -1,1 +1,19 @@
+package Java8.Basic.Lambda;
 
+import java.util.function.Supplier;
+public class RandomOTP{
+public static void main(String [] args){
+    Supplier<String> s = () -> {
+              String otp = "";
+              for(int i=1;i<4;i++){
+                  otp = otp +(int)(math.random()*10);
+              }
+          return otp;
+        }
+       System.out.println(s.get());
+	     System.out.println(s.get());
+	     System.out.println(s.get());
+	     System.out.println(s.get());
+	     System.out.println(s.get());
+}
+}
