6faf9c415937616be29c24e1ba7ad1da53972760
n1-NISHANT <nk98nishant@gmail.com>
2025-02-04 20:59:49 +0530

    Update Employee.java
    creating EmployeeDetails class to add fields and initialized variable with parametrized constructor with the help of this keyword.

--- Basic/Lambda/Employee.java
+++ Basic/Lambda/Employee.java
@@ -1,6 +1,17 @@
 package Java8.Basic.Lambda;
 
 
+class EmployeeDetails{
+  String name;
+	String location;
+	int sal;
+  
+    public EmployeeDetails(String name,String location,int sal) {
+		this.name = name;
+		this.location = location;
+		this.sal = sal;
+	}
+}
 public class Employee{
 public static void main(String [] args){
 
