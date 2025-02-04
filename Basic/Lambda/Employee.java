48de2ce79fd76340d1dce0afb7a6269314022f34
n1-NISHANT <nk98nishant@gmail.com>
2025-02-04 21:15:28 +0530

    Update Employee.java
    creating collection of Employee with the help of Arrays

--- Basic/Lambda/Employee.java
+++ Basic/Lambda/Employee.java
@@ -1,5 +1,6 @@
 package Java8.Basic.Lambda;
-
+import java.util.Arrays;
+import java.util.List;
 
 class EmployeeDetails{
   String name;
@@ -14,7 +15,13 @@ class EmployeeDetails{
 }
 public class Employee{
 public static void main(String [] args){
+	EmployeeDetails emp1 = new EmployeeDetails("Raju","pune",50000);
+	EmployeeDetails emp2 = new EmployeeDetails("Sohan","Delhi",20000);
+	EmployeeDetails emp3 = new EmployeeDetails("Prakash","Gurugram",75000);
+	EmployeeDetails emp4 = new EmployeeDetails("Vivek","Delhi",100000);
+	EmployeeDetails emp5 = new EmployeeDetails("Sandeep","puna",20000);
 
+	 List<EmpDetails> L1 = Arrays.asList(emp1,emp2,emp3,emp4,emp5);
   
 }
 }
