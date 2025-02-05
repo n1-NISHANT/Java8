5f8889160a81f35d3b75d938cf4d67377c47498d
n1-NISHANT <nk98nishant@gmail.com>
2025-02-05 19:04:57 +0530

    Update StringJoinerDemo.java

--- Basic/OtherUpdates/StringJoinerDemo.java
+++ Basic/OtherUpdates/StringJoinerDemo.java
@@ -1,1 +1,16 @@
+package Java8.Basic.OtherUpdates;
 
+import java.util.StringJoiner;
+public class StringJoinerDemo{
+public static void main(String[] args) {
+	 StringJoiner sj = new StringJoiner("-");
+	 sj.add("Nishant");
+	 sj.add("Kumar"); 
+	 System.out.println(sj);
+	 
+	 StringJoiner sj2 = new StringJoiner("-","(",")");
+	 sj2.add("Nishant");
+	 sj2.add("Kumar"); 
+	 System.out.println(sj2);
+ }
+}
