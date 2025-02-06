f9635b7901ae223e769592f5e0cf96085174507d
n1-NISHANT <nk98nishant@gmail.com>
2025-02-06 19:31:43 +0530

    Update InstanceMethodRef.java

--- Basic/OtherUpdates/InstanceMethodRef.java
+++ Basic/OtherUpdates/InstanceMethodRef.java
@@ -1,6 +1,15 @@
 package Java8.Basic.OtherUpdates;
 
 public class InstanceMethodRef{
-
-
+   public void m1() {
+		    for(int i = 0;i<=5;i++) {
+            System.out.println(i);
+		    }
+	   }
+	  public static void main(String[] args) {
+  		InstanceMethodRef im = new InstanceMethodRef();
+  		Runnable r = im::m1;
+  		Thread t = new Thread(r);
+		    t.start();
+	  }
 }
