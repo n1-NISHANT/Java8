1a9a1127b16ae51cae95a22794902ec16af6dd53
n1-NISHANT <nk98nishant@gmail.com>
2025-02-06 19:25:58 +0530

    Update CunstructorRef.java

--- Basic/OtherUpdates/CunstructorRef.java
+++ Basic/OtherUpdates/CunstructorRef.java
@@ -1,12 +1,15 @@
 package Java8.Basic.OtherUpdates;
-
+import java.util.function.Supplier;
 
 class Doctor{
-	public Doctor() {
-		System.out.println("Doctor Cunstructor........");
-	}
+   public Doctor() {
+	System.out.println("Doctor Cunstructor........");
+   }
 }
 public class CunstructorRef{
-
-
-  ]
+public static void main(String[] args) {
+    Supplier<Doctor> s = Doctor::new;
+    Doctor doctor = s.get();
+    System.out.println(doctor.hashCode());
+}
+}
