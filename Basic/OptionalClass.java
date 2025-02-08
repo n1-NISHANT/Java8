ca0c229cd1ba6b31a36a2648fcee8fc480863de9
n1-NISHANT <nk98nishant@gmail.com>
2025-02-08 21:18:04 +0530

    Update OptionalClass.java
    Creating a class User in this class one method available that is getUsername and its return type is Optional it checks enter userId match with any user or not and return Optional.ofNullable(string) [If it is match, then return match value to the caller. If it does not match, then return empty Optional.]

--- Basic/OptionalClass.java
+++ Basic/OptionalClass.java
@@ -1,1 +1,21 @@
+package Java8.Basic;
+
+import java.util.Optional;
+
+public class User {
+	public Optional<String> getUsername(Integer id){
+		String name =null;
+		if(id == 100) {
+			name = "Vikash";
+		}else if(id == 101) {
+			name = "Raju";
+		}else if(id == 102) {
+			name = "Jons";
+		}
+		return Optional.ofNullable(name);
+	}
+}
+public class OptionalClassDemo {
+	public static void main(String[] args) {
+}
 
