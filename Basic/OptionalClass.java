cdd7a83b2ee33c36e15f8ae21b57e2aeb3983865
n1-NISHANT <nk98nishant@gmail.com>
2025-02-08 21:22:05 +0530

    Update OptionalClass.java
    By using Scanner input from console and creating object of User Calss, then call User class method getUsername(int).

--- Basic/OptionalClass.java
+++ Basic/OptionalClass.java
@@ -1,7 +1,7 @@
 package Java8.Basic;
 
 import java.util.Optional;
-
+import java.util.Scanner;
 public class User {
 	public Optional<String> getUsername(Integer id){
 		String name =null;
@@ -17,5 +17,12 @@ public class User {
 }
 public class OptionalClassDemo {
 	public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	System.out.println("Enter User ID ");
+	int userId = s.nextInt();
+	
+	User u = new User();
+	
+	Optional<String> username = u.getUsername(userId);	
 }
 
