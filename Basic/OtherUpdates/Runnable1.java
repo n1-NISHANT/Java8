a562a524a3eaefaa1a1124b5dd7e308b976ef3a5
n1-NISHANT <nk98nishant@gmail.com>
2025-02-10 20:31:52 +0530

    Update Runnable1.java

--- Basic/OtherUpdates/Runnable1.java
+++ Basic/OtherUpdates/Runnable1.java
@@ -1,1 +1,17 @@
+package java8.Basic.OtherUpdates;
+
+public class Runnable1 implements Runnable {
+//write a java program to print number from 1 to 5 using Thread with the help of runnable interface.
+	@Override
+	public void run() {
+		for(int i = 1;i<=5;i++) {
+			System.out.println(i);
+		}
+	}
+	public static void main(String[] args) {
+		PrintNumber pn = new PrintNumber();
+		Thread th = new Thread(pn);
+		th.start();
+	}
+}
 
