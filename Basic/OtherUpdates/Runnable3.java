ee0ada7739abda955534d6e286fb180f9c2b9f02
n1-NISHANT <nk98nishant@gmail.com>
2025-02-10 20:41:14 +0530

    Update Runnable3.java

--- Basic/OtherUpdates/Runnable3.java
+++ Basic/OtherUpdates/Runnable3.java
@@ -1,1 +1,13 @@
+package Java8.Basic.OtherUpdates;
 
+public class Runnable3{
+public static void main(String[] args) {
+	Runnable rn  = ()->{
+	    	 for(int i=1;i<=5;i++) {
+	    		 System.out.println(i);
+	    	 }
+	     };
+	     Thread th = new Thread(rn);
+	     th.start();
+   }
+}
