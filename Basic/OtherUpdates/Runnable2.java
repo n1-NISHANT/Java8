f68d2dd5c18bc653c47d312ec04103c324ef07cc
n1-NISHANT <nk98nishant@gmail.com>
2025-02-10 20:37:40 +0530

    Update Runnable2.java
    By using anonyms inner class.

--- Basic/OtherUpdates/Runnable2.java
+++ Basic/OtherUpdates/Runnable2.java
@@ -1,1 +1,17 @@
+package Java8.Basic.OtherUpdates;
 
+public class Runnable2{
+public static void main(String[] args) {
+		Runnable rn = new Runnable() {
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				for(int i =1;i<=5;i++) {
+					System.out.println(i);
+				}
+			}
+		};
+		Thread th = new Thread(rn);
+		th.start();
+}
+}
