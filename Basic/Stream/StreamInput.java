7ebfb998e055fa266fb15828fd6fed07c56beac3
n1-NISHANT <nk98nishant@gmail.com>
2025-02-13 22:00:02 +0530

    Update StreamInput.java
    input stream with the help of, of() method.

--- Basic/Stream/StreamInput.java
+++ Basic/Stream/StreamInput.java
@@ -1,6 +1,10 @@
 package java8.Basic.Stream;
+import java.util.stream.Stream;
 
 public class StreamInput{
+public static void main(String[] args) {
+    Stream<Integer> stream =  Stream.of(10,20,30,40,50);
+		//System.out.println(stream);
 
-
+  }
 }
