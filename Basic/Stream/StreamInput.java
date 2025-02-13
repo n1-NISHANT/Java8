0bb8500a01e088f3d7db7f532ccc610b99191314
n1-NISHANT <nk98nishant@gmail.com>
2025-02-13 22:01:43 +0530

    Update StreamInput.java

--- Basic/Stream/StreamInput.java
+++ Basic/Stream/StreamInput.java
@@ -6,5 +6,11 @@ public static void main(String[] args) {
     Stream<Integer> stream =  Stream.of(10,20,30,40,50);
 		//System.out.println(stream);
 
+	ArrayList<String> al = new ArrayList<String>();
+	    al.add("Sam");
+	    al.add("Som");
+	    al.add("Karan");
+	    Stream<String> stream2 = al.stream();
+	    //System.out.println(stream2);
   }
 }
